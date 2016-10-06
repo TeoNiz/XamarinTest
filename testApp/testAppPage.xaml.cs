9f5aad1fd27c58997929174fb56ef0f0e4bd11e9
Teoeo <teodor.nizynski@gmail.com>
2016-10-06 15:48:23 +0200

    First commit

---
+++ testApp/testAppPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using Xamarin.Forms;
+
+namespace testApp
+{
+	public partial class testAppPage : ContentPage
+	{
+		public testAppPage()
+		{
+			InitializeComponent();
+			Content = new StackLayout
+			{
+				VerticalOptions = LayoutOptions.Center,
+				HorizontalOptions = LayoutOptions.Center,
+				Children ={
+					new Label{HorizontalTextAlignment=TextAlignment.Center, Text="Wircomen"},
+					new Button{Text="Klik meh"},
+					new Label{Text="0"}
+				}
+
+			};
+		}
+	}
+}
